d32dc3609a5314d18a3f39e031ad505b9117c208
HDS07 <hs4205170@gmail.com>
2024-10-02 17:16:14 +0530

    events done

--- Events/index.js
+++ Events/index.js
@@ -1,8 +1,23 @@
-var text=document.getElementById('fpara');
+// for index file
 
-function changetext(){
-    text.textContent="Nice, everything is going allright!!"
-}
-text.addEventListener('click',changetext)
+// var text=document.getElementById('fipara');
+
+// function changetext(){
+//     text.textContent="Nice, everything is going allright!!"
+// }
+// text.addEventListener('click',changetext)
 
 // text.removeEventListener('click',changetext)
+
+
+
+// for second file
+
+let para=document.querySelectorAll('p');
+function alertfun(event){
+    alert("Click on the para:"+ event.target.textContent);
+}
+for(let i=0;i<para.length;i++){
+    let paras=para[i];
+    paras.addEventListener('click',alertfun);
+}
